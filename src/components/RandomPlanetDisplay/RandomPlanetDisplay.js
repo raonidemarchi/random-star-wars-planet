a07fbb1336ce530bc1ebe0a40344211933657e26
Raoni Demarchi <raonidemarchi@gmail.com>
2019-08-16 11:10:58 -0300

    Apply useCallback

--- src/components/RandomPlanetDisplay/RandomPlanetDisplay.js
+++ src/components/RandomPlanetDisplay/RandomPlanetDisplay.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import Card, { CardTitle, CardBody, CardFooter } from '../Card/Card';
 import Button from '../Button/Button';
 import styles from './RandomPlanetDisplay.module.scss';
@@ -8,7 +8,40 @@ function RandomPlanetDisplay() {
   const [planetsCount, setPlanetsCount] = useState(0);
   const [planet, setPlanet] = useState({});
   const [featuredFilms, setFeaturedFilms] = useState([]);
-  const [loadingPlanets, setLoadingPlanets] = useState(true);
+  // const [loadingPlanets, setLoadingPlanets] = useState(true);
+
+  const fetchPlanetsCount = async () => {
+    const response = await fetch('https://swapi.co/api/planets/');
+    const result = await response.json();
+
+    setPlanetsCount(result.count);
+  }
+
+  const fetchRandomPlanet = async (planetsCount) => {
+    const randomId = Math.floor((Math.random() * planetsCount) + 1);
+    const response = await fetch(`https://swapi.co/api/planets/${randomId}/`);
+    const result = await response.json();
+
+    setPlanet(result);
+  }
+
+  const getFilmsTitle = async (planet, _fetchFilm) => {
+    const filmsArray = planet.films;
+    let filmsTitleArray = [];
+
+    if (filmsArray.length > 0) {
+      filmsTitleArray = await Promise.all(filmsArray.map(film => _fetchFilm(film)));
+    }
+
+    setFeaturedFilms(filmsTitleArray);
+  }
+
+  const fetchFilm = useCallback(async (filmUrl) => {
+    const response = await fetch(filmUrl);
+    const result = await response.json();
+
+    return result.title;
+  }, [])
 
   useEffect(() => {
     fetchPlanetsCount();
@@ -22,42 +55,10 @@ function RandomPlanetDisplay() {
 
   useEffect(() => {
     if (Object.entries(planet).length > 0) {
-      getFilmsTitle(planet);
+      getFilmsTitle(planet, fetchFilm);
+      document.title = planet.name;
     }
-  }, [planet]);
-
-  async function fetchPlanetsCount() {
-    const response = await fetch('https://swapi.co/api/planets/');
-    const result = await response.json();
-
-    setPlanetsCount(result.count);
-  }
-
-  async function fetchRandomPlanet(planetsCount) {
-    const randomId = Math.floor((Math.random() * planetsCount) + 1);
-    const response = await fetch(`https://swapi.co/api/planets/${randomId}/`);
-    const result = await response.json();
-
-    setPlanet(result);
-  }
-
-  async function getFilmsTitle(planet) {
-    const filmsArray = planet.films;
-    let filmsTitleArray = [];
-
-    async function _fetchFilm(filmUrl) {
-      const response = await fetch(filmUrl);
-      const result = await response.json();
-  
-      return result.title;
-    }
-
-    if (filmsArray.length > 0) {
-      filmsTitleArray = await Promise.all(filmsArray.map(film => _fetchFilm(film)));
-    }
-
-    setFeaturedFilms(filmsTitleArray);
-  }
+  }, [planet, fetchFilm]);
 
   return (
     <div className={styles.container}>
