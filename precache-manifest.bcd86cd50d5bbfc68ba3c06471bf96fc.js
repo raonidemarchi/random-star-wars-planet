97a4576628f0be428d0b8f5b7f0bc0ff889eca30
Raoni Demarchi <raonidemarchi@gmail.com>
2019-08-18 15:21:51 -0300

    Updates

---
+++ precache-manifest.bcd86cd50d5bbfc68ba3c06471bf96fc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4da720d47d7610f408db1b7a34525954",
+    "url": "/random-star-wars-planet/index.html"
+  },
+  {
+    "revision": "2c8722add17a07290b43",
+    "url": "/random-star-wars-planet/static/css/main.b6d0968d.chunk.css"
+  },
+  {
+    "revision": "ff9a80d6b98ab0d36186",
+    "url": "/random-star-wars-planet/static/js/2.844dd434.chunk.js"
+  },
+  {
+    "revision": "2c8722add17a07290b43",
+    "url": "/random-star-wars-planet/static/js/main.3007bd57.chunk.js"
+  },
+  {
+    "revision": "d854c0cebd942e205de1",
+    "url": "/random-star-wars-planet/static/js/runtime~main.bf1fa5af.js"
+  },
+  {
+    "revision": "e43cf385a0b644f2f1149a2c7f8384b0",
+    "url": "/random-star-wars-planet/static/media/death-star.e43cf385.svg"
+  },
+  {
+    "revision": "528d93d0b4bdbc73834c78242350f860",
+    "url": "/random-star-wars-planet/static/media/star-wars-logo.528d93d0.png"
+  }
+]);
